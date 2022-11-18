63a21d0f0ee04a29d0e07dada8d32cbd68f42854
Karlerikkanal <73522629+Karlerikkanal@users.noreply.github.com>
2022-11-18 20:46:28 +0200

    Added meta shop

---
+++ Assets/Scripts/UpgradePresenter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradePresenter : MonoBehaviour
+{
+    public TextMeshProUGUI CostText;
+    public TextMeshProUGUI CountText;
+    public Image Icon;
+    private Button button;
+}
